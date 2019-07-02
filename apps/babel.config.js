c0165b901541e0d7616012675f89a071074bdc60
David Viegas <dadviegas@hotmail.com>
2019-07-02 14:34:12 +0100

    add apps and webpack

---
+++ apps/babel.config.js
@@ -0,0 +1,15 @@
+module.exports = (api) => {
+  api.cache(true);
+
+  const presets = [
+    ['@babel/preset-env'],
+  ];
+  const plugins = [];
+  const overrides = [];
+
+  return {
+    overrides,
+    presets,
+    plugins,
+  };
+};
