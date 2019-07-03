2a4611affddbb3498f1d794b39877b063d5ac3a1
David Viegas <dadviegas@hotmail.com>
2019-07-03 12:23:05 +0100

    react conf

--- apps/web/babel.config.js
+++ apps/web/babel.config.js
@@ -2,9 +2,9 @@ module.exports = (api) => {
   api.cache(true);
 
   const presets = [
-    '@babel/preset-env',
+    '@babel/preset-env', '@babel/preset-react',
   ];
-  const plugins = [];
+  const plugins = ['@babel/plugin-proposal-class-properties'];
   const overrides = [];
 
   return {
--- apps/web/src/index.js
+++ apps/web/src/index.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import Middleware from 'asgard-middleware';
 
 import App from './components/Main/App';
 
+const middleware = new Middleware();
+middleware.run(() => console.log(1))
 // eslint-disable-next-line no-undef
 const wrapper = window.document.getElementById('root');
 
 ReactDOM.render(<App />, wrapper);
-
